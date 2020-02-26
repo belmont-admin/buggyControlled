f3cf0f754e94e161ec38a201d746c992b7eb1b99
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-02-26 13:31:15 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        kitronik_servo_lite.forward()
+    } else if (receivedNumber == 2) {
+        kitronik_servo_lite.left()
+    } else if (receivedNumber == 3) {
+        kitronik_servo_lite.right()
+    } else {
+        kitronik_servo_lite.stop()
+    }
 })
+basic.showIcon(IconNames.Heart)
+radio.setGroup(13)
